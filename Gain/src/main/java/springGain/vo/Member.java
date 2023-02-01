e132b08cf600dbcd37275e398a08d811a0554b66
YooMinWoo <alsn0527@naver.com>
2023-02-01 19:09:34 +0900

    과제

---
+++ Gain/src/main/java/springGain/vo/Member.java
@@ -0,0 +1,26 @@
+package springGain.vo;
+
+public class Member {
+	private String id;
+	private String pass;
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+	public Member(String id, String pass) {
+		this.id = id;
+		this.pass = pass;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+}
