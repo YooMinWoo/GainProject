3a3b5a1b0f51e757567ebc4c38ddedb6ba241059
som525 <som_525@naver.com>
2023-02-06 23:24:26 +0900

    T.T

--- Gain/src/main/java/springGain/service/CouponService.java
+++ Gain/src/main/java/springGain/service/CouponService.java
@@ -12,9 +12,9 @@ import springGain.vo.Coupon;
 public class CouponService {
 	@Autowired(required = false)
 	private CouponDao dao;
-	public List<Coupon> getCoupon(Coupon sch){
+	public List<Coupon> getCouponList(Coupon sch){
 		if(sch.getCouponName()==null) sch.setCouponName("");
-		return dao.getCoupon(sch);
+		return dao.getCouponList(sch);
 	}
 	
 }
