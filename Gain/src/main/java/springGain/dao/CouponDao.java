4431ae05a9ee720d2faa11a4ffda715f0956ac9c
som525 <som_525@naver.com>
2023-02-07 14:34:03 +0900

    ㅋㅍ

--- Gain/src/main/java/springGain/dao/CouponDao.java
+++ Gain/src/main/java/springGain/dao/CouponDao.java
@@ -5,5 +5,16 @@ import java.util.List;
 import springGain.vo.Coupon;
 
 public interface CouponDao {
+	
+	// 어드민 쿠폰 조회
 	public List<Coupon> getCouponList(Coupon sch);
+	
+	// 고객 마이 쿠폰 조회
+	
+	
+	// 쿠폰 등록
+	public void addCoupon(Coupon cp);
+	
+	// 쿠폰 삭제
+
 }
