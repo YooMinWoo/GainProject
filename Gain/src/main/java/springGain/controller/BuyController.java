b9a2b42733a79896e88d6763fdb48e827177bc96
miok0428 <gimd23214@gmail.com>
2023-02-07 22:37:31 +0900

    ss

--- Gain/src/main/java/springGain/controller/BuyController.java
+++ Gain/src/main/java/springGain/controller/BuyController.java
@@ -3,20 +3,19 @@ package springGain.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import springGain.service.BuyService;
-import springGain.vo.GainMember;
 
-@Controller
+@Controller("bctrl")
 public class BuyController {
 	@Autowired(required = false)
 	private BuyService service;
 	
 //	http://localhost:7080/Gain/getbuyList.do	
-	@RequestMapping("/getbuyList.do")
-	public String getbuyList(@ModelAttribute("id") String id,Model d) {
+	@GetMapping("/getbuyList.do/{id}")
+	public void getbuyList(@PathVariable("id") String id,Model d) {
 		 d.addAttribute("mlist", service.getGainMemberList(id));
 		
 		return "WEB-INF\\dongeunView\\buy01.jsp";
--- Gain/src/main/webapp/WEB-INF/dongeunView/buy01.jsp
+++ Gain/src/main/webapp/WEB-INF/dongeunView/buy01.jsp
@@ -107,11 +107,13 @@ $(document).ready(function(){
 				
 				<form>
 					<div id="sform01">
+						<c:forEach var="m01" items="${mlist}">
 						<input type="text" name="address"
-							placeholder="경기 성남시 분당구 백현동 565-6 그레이스빌" style="width: 55%"><br>
-						<input type="text" name="username" placeholder="김동은"
-							style="width: 10%"> <input type="text" name="phoneno"
-							placeholder="전화번호/폰번호"><br> <br> 배송메시지<br>
+							placeholder="경기 성남시 분당구 백현동 565-6 그레이스빌" style="width: 55%" value="${m01.address}"><br>
+						<input type="text" name="name" style="width: 10%" value="${m01.name }"> 
+							<input type="text" name="phonenum" value="${m01.phonenum }">
+						</c:forEach>
+							<br><br> 배송메시지<br>
 						<textarea placeholder="내용을 입력해주세요" style="width: 55%"></textarea>
 						<br> <br>
 					</div>
